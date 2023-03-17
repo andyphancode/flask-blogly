704fde75020e28994060e347f07e07c1ab0dab3f
andyphancode <andyphanpxh@gmail.com>
2023-03-17 05:51:22 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, redirect, render_template
+from models import db, connect_db, User
+from app import *
 
 app = Flask(__name__)
+app.app_context().push()
+app.config['SECRET_KEY'] = 'idksecretkey'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
 db.create_all()
+
+@app.route('/')
+def home():
+    return redirect("/users")
+
+@app.route('/users')
+def users():
+
+    users=User.query.all()
+    return render_template('users.html', users=users)
