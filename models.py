73da2d9ab75042ff320942bb7c4a67fdc090ec84
andyphancode <andyphanpxh@gmail.com>
2023-03-17 05:51:19 -0700

    Update models.py

--- models.py
+++ models.py
@@ -9,3 +9,20 @@ def connect_db(app):
 
     db.app = app
     db.init_app(app)
+
+class User(db.Model):
+    """User."""
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    first_name = db.Column(db.Text,
+                           nullable=False)
+    last_name = db.Column(db.Text,
+                          nullable=False)
+    image_url = db.Column(db.Text,
+                          nullable=False,
+                          default='https://cdn.discordapp.com/emojis/1086145090667421717?size=96&quality=lossless')
+    
