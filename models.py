a2c7b656f2f8ff7f9a60c3875b0e77c4e3cec0a9
andyphancode <andyphanpxh@gmail.com>
2023-03-19 17:15:00 -0700

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 """Models for Blogly."""
 
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 db = SQLAlchemy()
 
@@ -26,8 +27,37 @@ class User(db.Model):
                           nullable=False,
                           default='https://cdn.discordapp.com/emojis/1086145090667421717?size=96&quality=lossless')
     
+    posts = db.relationship("Post", backref="user", cascade="all, delete-orphan")
+
     @property
     def full_name(self):
-        "Return full name"
+
+        """Return full name"""
         
         return f"{self.first_name} {self.last_name}"
+
+class Post(db.Model):
+    """Post."""
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    title = db.Column(db.Text,
+                      nullable=False)
+    content = db.Column(db.Text,
+                        nullable=False)
+    created_at = db.Column(db.DateTime,
+                           nullable=False,
+                           default=datetime.datetime.now)
+    user_id = db.Column(db.Integer,
+                        db.ForeignKey('users.id'),
+                        nullable=False)
+
+    @property
+    def friendly_date(self):
+
+        """Nicer date format"""
+
+        return self.created_at.strftime("%a %b %-d  %Y, %H:%M:%S")
